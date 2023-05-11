5c43602a82ddb57fa0db77f02420d17673820117
yubs <yubs@bsfit.com.cn>
2023-05-11 20:59:35 +0800

    feat:增加：寻找符合要求的最长子串

---
+++ nowcoder/src/test/java/site/bleem/nowcoder/FindMaxLengthStrTest.java
@@ -0,0 +1,23 @@
+package site.bleem.nowcoder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindMaxLengthStrTest {
+    /**
+     *  示例1
+     *     输入
+     *         ABC123
+     *     输出：6
+     *  示例2
+     *     输入
+     *         ABACA123D
+     *     输出：7
+     */
+    @Test
+    public void test(){
+        Assert.assertEquals("6",FindMaxLengthStr.executor("D","ABC123"));
+        Assert.assertEquals("7",FindMaxLengthStr.executor("D","ABACA123D"));
+    }
+
+}
